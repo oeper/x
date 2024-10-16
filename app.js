614faeab340c8c00e1b2deca27875bee4dcc5624
oeper <82303578+oeper@users.noreply.github.com>
2024-10-16 22:50:35 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+    import Tweets from './components/Tweets/Tweets';
+
+    const App = () => {
+      return (
+        <div>
+          <h1>Twitter Clone</h1>
+          <Tweets />
+        </div>
+      );
+    };
+
+    export default App;
